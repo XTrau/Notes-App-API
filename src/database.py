2b730c69ed2b492b237f235b6ea93360f68e5988
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2024-07-12 22:09:26 +0500

    Init

---
+++ src/database.py
@@ -0,0 +1,25 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from config import settings
+
+engine = create_async_engine(url=settings.database_url_asyncpg, echo=True)
+new_session = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+Base = declarative_base()
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with new_session() as session:
+        yield session
---
+++ src/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from database import drop_tables, create_tables
+
+app = FastAPI()
+
+allow_headers = [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+    "User-Agent",
+    "DNT",
+    "Cache-Control",
+    "X-Mx-ReqToken",
+    "Keep-Alive",
+    "X-Requested-With",
+    "If-Modified-Since",
+    "X-CSRF-Token"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allow_headers=allow_headers
+)
+
+
+@app.get("/")
+async def root():
+    try:
+        await create_tables()
+    except Exception as e:
+        return {"error": "Failed connect to database"}
+    return {"Status": "Server Working!"}
+
+
+@app.post("/reset_db")
+async def reset_database():
+    await drop_tables()
+    await create_tables()
+    return {"message": "Database reseted!"}
